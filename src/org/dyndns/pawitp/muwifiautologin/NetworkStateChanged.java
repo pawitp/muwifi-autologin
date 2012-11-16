86308004d359ce9722ade529bf392379f88555c4
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-11-16 13:38:12 +0700

    Support Android 4.2

--- src/org/dyndns/pawitp/muwifiautologin/NetworkStateChanged.java
+++ src/org/dyndns/pawitp/muwifiautologin/NetworkStateChanged.java
@@ -13,9 +13,11 @@ public class NetworkStateChanged extends BroadcastReceiver {
 
     static final String TAG = "NetworkStateChanged";
     static final String SSID = "MU-WiFi";
+    static final String SSID2 = "\"MU-WiFi\""; // JB 4.2 quotes SSID
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive");
         // Check preference
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         if (!prefs.getBoolean(Preferences.KEY_ENABLED, false)) {
@@ -26,22 +28,18 @@ public class NetworkStateChanged extends BroadcastReceiver {
 
         // Check network connected
         NetworkInfo netInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+        Log.d(TAG, "onReceive: netInfo = " + netInfo);
         if (!netInfo.isConnected()) {
             return;
         }
 
         // Check SSID
-        WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-        try {
-            if (!wifi.getConnectionInfo().getSSID().equalsIgnoreCase(SSID)) {
-                return;
-            }
-        }
-        catch (NullPointerException e) {
-            // So many things can be null here when network is not connected
+        String ssid = netInfo.getExtraInfo();
+        Log.d(TAG, "onReceive: SSID = " + ssid);
+        if (!ssid.equalsIgnoreCase(SSID) && !ssid.equalsIgnoreCase(SSID2)) {
+            Log.d(TAG, "Invalid SSID");
             return;
         }
-
 
         Log.v(TAG, "Connected to the correct network");
 
